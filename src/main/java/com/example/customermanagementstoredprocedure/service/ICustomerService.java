d6e41cdc7e4aa1447a4d28e4bc93ce0af443205b
NguyenVanAnh98 <nguyenanhtk321@gmail.com>
2024-05-20 16:06:02 +0700

    first commit

---
+++ src/main/java/com/example/customermanagementstoredprocedure/service/ICustomerService.java
@@ -0,0 +1,9 @@
+package com.example.customermanagementstoredprocedure.service;
+
+
+import com.example.customermanagementstoredprocedure.model.Customer;
+
+
+public interface ICustomerService {
+    boolean saveWithStoredProcedure(Customer customer);
+}
